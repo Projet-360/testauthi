88a707f5acb3f6cd5d941447da7faedd2349d4c4
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-07 15:15:29 +0200

    tset

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
 import type { LayoutServerLoad } from "./$types"
 
 export const load: LayoutServerLoad = async (event) => {
-  return {
-    session: await event.locals.getSession(),
+  const session = await event.locals.getSession()
+  if (!session && event.url.pathname.startsWith('/protected')) {
+    return new Response('Not authenticated', { status: 401 });
   }
+  return { session }
 }
