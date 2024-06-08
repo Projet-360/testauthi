ef75c9eddd0270ce5afb7879f6ced015bcadc8a3
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-08 18:50:11 +0200

    change base files

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,66 @@
-
-
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import { defineConfig } from 'vite';
+import sveltePreprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import sass from 'vite-plugin-sass';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: vercel(),
-    alias: {
-      $components: "src/components",
-      $lib: "src/components",
-      $routes: "src/routes",
-    },
-  },
-}
-
-export default config
+export default defineConfig({
+	plugins: [
+		sass({ outputStyle: 'compressed' }),
+		// VitePWA({
+		// 	manifest: {
+		// 		// les options pour votre manifeste
+		// 		name: 'Mon App SvelteKit PWA',
+		// 		short_name: 'SvelteKitPWA',
+		// 		description: 'Une démo de PWA avec SvelteKit et Vite',
+		// 		icons: [
+		// 			// Vos icônes pour la PWA
+		// 		]
+		// 		// ... d'autres options du manifeste
+		// 	},
+		// 	workbox: {
+		// 		// les options pour workbox
+		// 		swSrc: './src/sw.js',
+		// 		runtimeCaching: [
+		// 			{
+		// 				urlPattern: /^https?.*/,
+		// 				handler: 'NetworkFirst',
+		// 				options: {
+		// 					cacheName: 'http-cache'
+		// 				}
+		// 			}
+		// 		]
+		// 	}
+		// })
+	],
+	preprocess: [
+		sveltePreprocess({
+			scss: {
+				includePaths: ['src/styles']
+			},
+			postcss: {
+				plugins: [autoprefixer()]
+			}
+		})
+	],
+	kit: {
+		adapter: vercel(),
+		alias: {
+			// this will match a file
+			$db: 'src/db',
+			$components: 'src/components',
+			$modelNotifications: 'src/api/modelNotifications',
+			$utils: 'src/utils',
+			$stores: 'src/stores',
+			$UITools: 'src/lib/UITools',
+			$language: 'src/language',
+			$three: 'src/lib/components/three',
+			$services: 'src/services',
+			$lib: "src/components",
+			$api: 'src/api',
+			$routes: "src/routes"
+		}
+	}
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [sveltekit()],
-})
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+
+	resolve: {
+		preserveSymlinks: true
+	},
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "src/styles/main.scss"'
+			}
+		}
+	},
+
+	server: {
+		port: 1000
+	}
+};
+
+export default config;
