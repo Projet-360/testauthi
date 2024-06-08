596da3d5ffb7573157a136ba6e9520673b9f2661
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-08 17:35:02 +0200

    test with page login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,7 @@
-export { handle } from "./auth"
+// src/hooks.server.ts
+import { handle as authHandle } from './auth';
+import { sequence } from '@sveltejs/kit/hooks';
+
+export const handle = sequence(authHandle, async ({ event, resolve }) => {
+  return resolve(event);
+});
